2d21b6ac4e4987817ab051146625f07dd2ad42c8
1Aksoni <akshat2001soni@gmail.com>
2024-05-28 00:34:03 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,9 @@ import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 const app=express();
-const path = require("path");
+import path from "path";
 app.use(cors());
 
-    
     app.use(express.json({limit:"16kb"})); 
     app.use(urlencoded({extended:true,limit:"16kb"})); 
     app.use(express.static("public")) 
