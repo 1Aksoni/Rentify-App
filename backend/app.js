1e6adfa1355fcb455d6275ce5f5485c38a8ccc72
1Aksoni <akshat2001soni@gmail.com>
2024-05-28 00:50:57 +0530

    change static path

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app=express();
 import path from "path";
+import { fileURLToPath } from 'url';
+
+// Get the directory name from the URL
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
 app.use(cors());
 
     app.use(express.json({limit:"16kb"})); 
@@ -10,9 +15,9 @@ app.use(cors());
     app.use(express.static("public")) 
     
     app.use(cookieParser()) 
-    app.use(express.static(path.join(__dirname,'build')));
+    app.use(express.static(path.join(_dirname,'build')));
     app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname,'build', 'index.html'));     
+      res.sendFile(path.join(_dirname,'build', 'index.html'));     
     });
     //import routes
     import publicRoutes from './routes/public.routes.js';
